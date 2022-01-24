6b106d9e617b95c59094faa508c63901b7b05cd5
Madeline Sem <maddiesmac@Madelines-MacBook-Air.local>
2022-01-24 16:13:06 -0500

    Initial commit

---
+++ src/com/company/Main.java
@@ -0,0 +1,18 @@
+package com.company;
+
+public class Main {
+
+    public static void main(String[] args) {
+        int x = 5;
+        int y;
+        String s = "java";
+        boolean b = false;
+        double dbl = 3.145;
+        y = 4;
+        System.out.println("Hello Class");
+        System.out.print("Hello Class");
+        System.out.println(x);
+        System.out.println(y);
+        System.out.println(dbl);
+    }
+}
